413da076cb677f5e0ccb41e291ab20e0189642c5
greatlalbabu <greatlalbabu.2015@gmail.com>
2024-03-04 23:17:26 +0530

    first assignment done

--- Classtask/classtask1/class.js
+++ Classtask/classtask1/class.js
@@ -1,10 +1,22 @@
 
-const h1= React.createElement('h1', {}, "Learn Web Development")
-const P= React.createElement('p', {}, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta facilis quam mollitia magnam explicabo ipsum enim sed exercitationem adipisci sunt! Quod possimus repellendus neque? Consectetur reiciendis minus voluptate illo ab nobis, rerum numquam vel deserunt omnis maiores quaerat minima! Soluta ipsum sed fugit, suscipit beatae atque omnis tenetur voluptatem incidunt at, eum, nesciunt quis temporibus sunt.")
+const H1= React.createElement('h1', {}, "Learn Web Development")
+const P= React.createElement('p', {}, "Web development refers to the process of building and maintaining websites or web applications. It encompasses a variety of tasks and skills, ranging from creating simple static web pages to developing complex dynamic web applications. Web development involves both the front-end (client-side) and back-end (server-side) aspects of a website or application.")
 
-const P2= React.createElement('p', {}, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta facilis quam mollitia magnam explicabo ipsum enim sed exercitationem adipisci sunt! Quod possimus repellendus neque? Consectetur reiciendis minus voluptate illo ab nobis, rerum numquam vel deserunt omnis maiores quaerat minima! Soluta ipsum sed fugit, suscipit beatae atque omnis tenetur voluptatem incidunt at, eum, nesciunt quis temporibus sunt.")
+const P2 = React.createElement('p', {}, [
+    'Full-stack development refers to the practice of working on both the front-end (client-side) and back-end (server-side) portions of a web application. A full-stack developer is someone who possesses a skill set that allows them to handle tasks related to both the user interface and the server-side logic of a web application. This includes designing and building the user interface, implementing business logic, and managing the server and database. ',
+    React.createElement('a', { href: 'https://en.wikipedia.org/wiki/Web_development', key: 'link' }, 'Learn more about full-stack development.')
+  ]);
+  
 
-const Div = React.createElement('div',{ className : 'div'},[h1, P, P2])
+const P3= React.createElement('p', {}, "Web development is a dynamic field, and developers often need to adapt to new technologies and frameworks. It's a broad discipline that covers a wide range of skills and tools to create efficient, secure, and visually appealing web solutions.")
 
 
-ReactDOM.render(Div, document.getElementById("root"))
+const Div = React.createElement('div',{},[P])
+const secDiv = React.createElement('div',{},[P2])
+const thirdDiv = React.createElement('div',{},[P3])
+ReactDOM.render(H1, document.getElementById("heading"))
+
+
+ReactDOM.render(Div, document.getElementById("container"))
+ReactDOM.render(secDiv, document.getElementById("para"))
+ReactDOM.render(thirdDiv, document.getElementById("third"))
